d08e119e6a834c8e1436e67c7d452281918b2004
joe <jguttenberg.ieu2024@student.ie.edu>
2026-02-17 17:11:15 +0100

    Class exercise

--- decode_start.py
+++ decode_start.py
@@ -1,6 +1,13 @@
+from itertools import count
+
 alphabet = " abcdefghijklmnopqrstuvwxyz"
 vowel = "aeiou"
 message = ""
 with open("secret.txt") as f:
     for line in f:
-        print(line) # instead of print, you should start decoding
+        num_vowles = 0
+        for v in vowel:
+            num_vowles = num_vowles + line.count(v)
+        letter = alphabet[num_vowles]
+        message += letter
+    print(message)
